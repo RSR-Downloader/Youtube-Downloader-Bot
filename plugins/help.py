cd461eb0062d9af2a8082d6f81fcfe8864aefdd4
RSR-Downloader <79363075+RSR-Downloader@users.noreply.github.com>
2021-02-20 23:06:41 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Aw le! Youtube Video Mp3/Mp4 engpawh ka download theia, Mahse, Playlists a theih loh thung. Youtube URL Link lo thawn tawp la aw. Chuan Mp3/Mp4 i duh ilo thlang mai ang. Hriatthiam loh i neih chuan Join @rsrmusic"
+    helptxt = f"Aw le! Youtube Video Mp3/Mp4 engpawh ka download theia, Mahse, Playlists a theih loh thung. Youtube URL Link lo thawn tawp la aw. Chuan Mp3/Mp4 i duh ilo thlang mai ang,chuan i awmna group ah min add a,admin a min dah bawk chuan group ah pawh hna ka thawk thei e,min add dan tur chu i group Add member tih ah khan kal la,mami4_bot ti in search la,ka lo lang anga min add mai dawn nia. Hriatthiam loh i neih chuan tah hian min lo be dawn nia @rsrmusic"
     await message.reply_text(helptxt)
